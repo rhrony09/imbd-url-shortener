923f75ad80d16c4bac3ea2a7cd05916f96281109
rhrony09 <rhrony0009@gmail.com>
2025-04-05 14:48:33 +0600

    remove crx

--- public/extension/install-imbd-url-extension.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-SET KEY="HKCU\Software\Google\Chrome\Extensions\fhaklhfjbhkkgfekaeklodfbcbndcmcj"
-REG ADD %KEY% /v "update_url" /t REG_SZ /d "https://imbdurl.com/extension/updates.xml" /f
-
-echo Extension installation initiated. Please restart Chrome.
-pause
